d1bde3fd558c2355b56d7ef778956524e1404e97
air <air@lush.co.kr>
2024-03-19 09:43:04 +0900

    콜럼버스 스터디 2일차 문제 풀이
    Q3. 특정 반복되는 개수가 여러개일때 '?' 그게 아니라면 해당 문자 출력
    
    Q4. 특정 문자열의 패턴과 일치하면 해당 패턴과 일치는지 하지 않는지 확인하는 문제

---
+++ src/ColumbusStudy/week1/Question3.java
@@ -0,0 +1,25 @@
+package ColumbusStudy.week1;
+
+import java.util.Scanner;
+
+public class Question3 {
+    // https://www.acmicpc.net/problem/1157
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next().toUpperCase();
+        int[] cnt = new int[26];
+        int max = 0;
+        char result = '?';
+
+        for (char x :  str.toCharArray()){
+            cnt[x-'A']++;
+            if(max < cnt[x - 'A']){
+                max = cnt[x-'A'];
+                result = x;
+            } else if(max == cnt[x -'A']){
+                result = '?';
+            }
+        }
+        System.out.println(result);
+    }
+}
