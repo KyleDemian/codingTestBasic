33aead3377e4401d584ccd6f22c849bff813f176
air <air@lush.co.kr>
2024-03-25 10:29:27 +0900

    콜럼버스 스터디 ( Week 2 ) > 문제 자체들은 다 쉬웠음 < Q1. (Queue) 사실 큐를 사용하는것보다 더 중요했던건 BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); StringBuilder sb = new StringBuilder();
    를 사용해서 데이터를 I/O 를 할 수 있느냐 없느냐가 더 중요했음.
    
    Q2. (Stack) 그냥 괄호 있는지 없는지 확인하는 문제

--- src/ColumbusStudy/week2/Question1.java
+++ src/ColumbusStudy/week2/Question1.java
@@ -1,4 +1,95 @@
 package ColumbusStudy.week2;
 
+import java.io.*;
+import java.util.*;
+
 public class Question1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+        int last = 0;
+        String[] arr = new String[n];
+        for(int i=0; i < n ; i++){
+            arr[i] = br.readLine();
+        }
+
+        for(int i=0; i < n ; i++){
+            StringTokenizer st = new StringTokenizer(arr[i]);
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    last = Integer.parseInt(st.nextToken());
+                    q.offer(last);
+                    break;
+                case "pop":
+                    if(q.isEmpty()) sb.append("-1\n");
+                    else sb.append(q.poll()).append("\n");
+                    break;
+                case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
+                    if(q.isEmpty()) sb.append("1\n");
+                    else sb.append("0\n");
+                    break;
+                case "front":
+                    if(q.isEmpty()) sb.append("-1\n");
+                    else sb.append(q.peek()).append("\n");
+                    break;
+                case "back":
+                    if(q.isEmpty()) sb.append("-1\n");
+                    else sb.append(last).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    /*
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = kb.nextInt();
+        kb.nextLine();
+        String[] arr = new String[n];
+        for(int i=0; i < arr.length ; i++){
+            arr[i] = kb.nextLine();
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int last = 0;
+
+        // n 값만큼 돌려야함.
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(arr[i]);
+            String cmd = st.nextToken();
+            if(arr[i].contains("push")){
+                last = Integer.parseInt(st.nextToken());
+                q.offer(last);
+            }else if(arr[i].equals("pop")){
+                if(q.isEmpty()) sb.append("-1").append("\n");
+                else sb.append(q.poll()).append("\n");
+            }else if(arr[i].equals("size")){
+                sb.append(q.size()).append("\n");
+            }else if(arr[i].equals("empty")){
+                if(q.isEmpty()) sb.append("1").append("\n");
+                else sb.append("0").append("\n");
+            }else if(arr[i].equals("front")){
+                if(q.isEmpty()) sb.append("-1").append("\n");
+                else sb.append(q.peek()).append("\n");
+            }else if(arr[i].equals("back")){
+                if(q.isEmpty()) sb.append("-1").append("\n");
+                else sb.append(last).append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+    */
+
 }
---
+++ src/ColumbusStudy/week2/Question2.java
@@ -0,0 +1,40 @@
+package ColumbusStudy.week2;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        kb.nextLine();
+        String[] arr = new String[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = kb.nextLine();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            Stack<Character> stack = new Stack<>();
+            boolean isValid = true;
+            for (char x : arr[i].toCharArray()) {
+                if(x == '('){
+                    stack.push(x);
+                } else {
+                    if(stack.isEmpty()){
+                        isValid = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+
+            if(!stack.isEmpty()) isValid = false;
+
+            if (isValid) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
