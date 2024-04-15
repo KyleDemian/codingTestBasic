a3aa7f96ff4820b3634764cebce3d3c2e695d45b
air <air@lush.co.kr>
2024-04-15 11:39:19 +0900

    콜럼버스 스터디 Review -D
    Q16, Q19번 아예 못품
    명일 할거 3주차 7번부터

---
+++ src/ColumbusStudy/reviewWeek/Q22.java
@@ -0,0 +1,34 @@
+package ColumbusStudy.reviewWeek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Q22 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        HashMap<String, Double> hm = new HashMap<>();
+        double total = 0.0;
+        String input;
+        while ((input = br.readLine()) != null && !input.equals("")) {
+            total++;
+            hm.put(input, hm.getOrDefault(input, 0.0) + 1.0);
+        }
+
+        Map<String, Double> sort = new TreeMap<>(hm);
+        for (Map.Entry<String, Double> entry : sort.entrySet()) {
+            String key = entry.getKey();
+            Double val = entry.getValue();
+            double percent = (val / total) * 100;
+
+            sb.append(key).append(" ").append(String.format("%.4f", percent)).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
