b2f20b5a262dab2d1a5475de048b9200fe10e0fa
air <air@lush.co.kr>
2024-04-22 14:22:52 +0900

    콜롬버스 스터디 2회차
    첫번째 주,
    Q1. 피보나치 수열 단, 0, 1, 2 를 입력 받았을때를 고려해야함.
    
    Q2. 특정 숫자값이 들어오면 해당 값을 칸토어 집합으로 만들면 됌,
    
    Q3. 선린인터넷고등학교 문제에서 전부다 답을 주어지고 있었고, 초기화 위치만 잘생각해서 달면 끝.

--- src/ColumbusStudy/week5/Question1.java
+++ src/ColumbusStudy/week5/Question1.java
@@ -14,18 +14,31 @@ public class Question1 {
         int n = Integer.parseInt(br.readLine());
 
         // 오답 이유, n 값이 0 또는 1, 2 일때의 경우를 생각하지 않았음.
-        int a = 1, b = 1, c = 0;
-        if (n == 0) {
-            System.out.println(0);
-        } else if (n == 1 || n == 2) {
-            System.out.println(1);
-        } else {
-            for (int i = 2; i < n; i++) {
-                c = a + b;
-                a = b;
-                b = c;
-            }
-            System.out.println(c);
-        }
+        // 기존에는 1 1 값을 가지고 시작햇지만, 여기서의 차수는 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 .....
+        // 문제에 대해서 오류가 있는거 같음 0 부터시작하면 값이 34가 10번째 항이고,
+        // 1부터 시작하면 55가 10번째 항임. 그래서 문제가 틀린것으로 보임.
+
+//        int a = 1, b = 1, c = 0;
+//        if (n == 0) {
+//            System.out.println(0);
+//        } else if (n == 1 || n == 2) {
+//            System.out.println(1);
+//        } else {
+//            for (int i = 2; i < n; i++) {
+//                c = a + b;
+//                a = b;
+//                b = c;
+//            }
+//            System.out.println(c);
+//        }
+
+        int result = fibo(n);
+        System.out.println(result);
+    }
+
+    static int fibo(int n){
+        if(n <= 0) return 0;
+        else if(n == 1 )  return 1;
+        else return fibo(n-1) + fibo(n-2);
     }
 }
