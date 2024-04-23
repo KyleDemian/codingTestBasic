db8f03a6da4ac154f1deb0144b8c1a8d4ca8ef9b
air <air@lush.co.kr>
2024-04-23 09:04:21 +0900

    콜롬버스 스터디 2회차
    첫번째 주,
    Q1. 피보나치 수열 단, 0, 1, 2 를 입력 받았을때를 고려해야함.
    
    Q2. 특정 숫자값이 들어오면 해당 값을 칸토어 집합으로 만들면 됌,
    
    Q3. 선린인터넷고등학교 문제에서 전부다 답을 주어지고 있었고, 초기화 위치만 잘생각해서 달면 끝.
    
    문제 주석 처리 및 복습 진행

--- src/ColumbusStudy/week5/Question1.java
+++ src/ColumbusStudy/week5/Question1.java
@@ -3,8 +3,11 @@ package ColumbusStudy.week5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Question1 {
+
+    static long[] dp = new long[25];
 
     // https://www.acmicpc.net/problem/10870
     public static void main(String[] args) throws IOException {
@@ -18,6 +21,7 @@ public class Question1 {
         // 문제에 대해서 오류가 있는거 같음 0 부터시작하면 값이 34가 10번째 항이고,
         // 1부터 시작하면 55가 10번째 항임. 그래서 문제가 틀린것으로 보임.
 
+        // 일반적인 피보나치 방식
 //        int a = 1, b = 1, c = 0;
 //        if (n == 0) {
 //            System.out.println(0);
@@ -31,14 +35,23 @@ public class Question1 {
 //            }
 //            System.out.println(c);
 //        }
-
-        int result = fibo(n);
+        Arrays.fill(dp, -1);
+        long result = fibo(n);
         System.out.println(result);
     }
 
-    static int fibo(int n){
-        if(n <= 0) return 0;
-        else if(n == 1 )  return 1;
-        else return fibo(n-1) + fibo(n-2);
+    // 동적계획법 으로 미리 배열에 데이터 저장하여 해당 데이터를 호출하는 방식
+    static long fibo(int n) {
+        if (n <= 1) return n;
+        if (dp[n] != -1) return dp[n];
+        dp[n] = fibo(n - 1) + fibo(n - 2);
+        return dp[n];
     }
+
+    // 그냥 재귀 호출 방식
+//    static int fibo(int n){
+//        if(n <= 0) return 0;
+//        else if(n == 1 )  return 1;
+//        else return fibo(n-1) + fibo(n-2);
+//    }
 }
--- src/ColumbusStudy/week5/Question2.java
+++ src/ColumbusStudy/week5/Question2.java
@@ -18,8 +18,11 @@ public class Question2 {
             char[] text = new char[(int) Math.pow(3, n)];
             Arrays.fill(text, '-');
 
+            // n 차 만큼의 횟수
             for(int i = 0; i < n; i++) {
+                // 1차 가운데 공백 횟수 만큼 공백 만듬 i+1
                 for(int j = 0; j < text.length; j += (int) Math.pow(3, i+1)) {
+                    // 그 공백의 시작과, 끝만큼 공백으로 변경
                     int start = j + (int) Math.pow(3, i);
                     int end = start + (int) Math.pow(3, i);
                     Arrays.fill(text, start, end, ' ');
